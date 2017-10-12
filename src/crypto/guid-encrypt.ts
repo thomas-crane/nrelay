5cd364bc034a73cbf6f37be6e2a44cc0c65e43b0
thomas-crane <thomasgcrane@gmail.com>
2017-10-12 17:14:30 +1100

    Minor formattting changes to guid-encrypt

--- src/crypto/guid-encrypt.ts
+++ src/crypto/guid-encrypt.ts
@@ -1,14 +1,15 @@
 import crypto = require('crypto');
 
 export function encryptGUID(guid: string): string {
-    const key = '-----BEGIN PUBLIC KEY-----\n' +
+    const key =
+    '-----BEGIN PUBLIC KEY-----\n' +
     'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDCKFctVrhfF3m2Kes0FBL/JFeO\n' +
     'cmNg9eJz8k/hQy1kadD+XFUpluRqa//Uxp2s9W2qE0EoUCu59ugcf/p7lGuL99Uo\n' +
     'SGmQEynkBvZct+/M40L0E0rZ4BVgzLOJmIbXMp0J4PnPcb6VLZvxazGcmSfjauC7\n' +
     'F3yWYqUbZd/HCBtawwIDAQAB\n' +
     '-----END PUBLIC KEY-----';
 
-    const encryptedGUID = crypto.publicEncrypt(key, Buffer.from(guid));
+    const encryptedGUID = crypto.publicEncrypt(key, Buffer.from(guid, 'utf8'));
 
     return encryptedGUID.toString('base64');
 }
