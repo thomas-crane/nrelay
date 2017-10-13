5c0b75acf7547775a50e0f110a2f8f6b90c2321c
thomas-crane <thomasgcrane@gmail.com>
2017-10-13 11:37:58 +1100

    Changes to mapinfo-packet.ts:
    Implemented write method.
    
    Fixed a bug where the difficulty property was being read too early and
    causing the bufferIndex to overrun the buffer when attempting to read
    the clientXML and extraXML properties.

--- src/networking/packets/incoming/mapinfo-packet.ts
+++ src/networking/packets/incoming/mapinfo-packet.ts
@@ -18,33 +18,43 @@ export class MapInfoPacket extends Packet {
     extraXML: string[];
     //#endregion
 
-    public read() {
+    public read(): void {
         this.width = this.readInt32();
         this.height = this.readInt32();
         this.name = this.readString();
         this.displayName = this.readString();
-        this.difficulty = this.readInt32();
         this.fp = this.readUInt32();
         this.background = this.readInt32();
         this.difficulty = this.readInt32();
         this.allowPlayerTeleport = this.readBoolean();
         this.showDisplays = this.readBoolean();
-
-        // in many cases (such as loading the nexus) attempting to
-        // read these will read past the end of the buffer. Since there
-        // is no protection (yet) just skip these properties.
-
-        // this.clientXML = new Array<string>(this.readShort());
-        // for (let i = 0; i < this.clientXML.length; i++) {
-        //     this.clientXML[i] = this.readStringUTF32();
-        // }
-        // this.extraXML = new Array<string>(this.readShort());
-        // for (let i = 0; i < this.extraXML.length; i++) {
-        //     this.extraXML[i] = this.readStringUTF32();
-        // }
+        this.clientXML = new Array<string>(this.readShort());
+        for (let i = 0; i < this.clientXML.length; i++) {
+            this.clientXML[i] = this.readStringUTF32();
+        }
+        this.extraXML = new Array<string>(this.readShort());
+        for (let i = 0; i < this.extraXML.length; i++) {
+            this.extraXML[i] = this.readStringUTF32();
+        }
     }
 
-    public write() {
-
+    public write(): void {
+        this.writeInt32(this.width);
+        this.writeInt32(this.height);
+        this.writeString(this.name);
+        this.writeString(this.displayName);
+        this.writeUInt32(this.fp);
+        this.writeInt32(this.background);
+        this.writeInt32(this.difficulty);
+        this.writeBoolean(this.allowPlayerTeleport);
+        this.writeBoolean(this.showDisplays);
+        this.writeShort(this.clientXML.length);
+        for (let i = 0; i < this.clientXML.length; i++) {
+            this.writeStringUTF32(this.clientXML[i]);
+        }
+        this.writeShort(this.extraXML.length);
+        for (let i = 0; i < this.extraXML.length; i++) {
+            this.writeStringUTF32(this.extraXML[i]);
+        }
     }
 }
