0ff161ad097002f435061cdb7fa43f3e2015acfb
thomas-crane <thomasgcrane@gmail.com>
2017-10-13 11:01:37 +1100

    Reworked packetio.ts
    Changed packetio to be constructed with an instance of net.Socket
    packetio.ts is now responsible for sending and receiving raw packet data
    and emitting events with packet structures.

--- src/networking/packetio.ts
+++ src/networking/packetio.ts
@@ -1,16 +1,48 @@
+import events = require('events');
+import net = require('net');
 import { Packet } from './packet';
+import { Packets } from './packets';
 import stream = require('stream');
-import { RC4, OUTGOING_KEY } from './../crypto/rc4';
+import { RC4, OUTGOING_KEY, INCOMING_KEY } from './../crypto/rc4';
+import { Log, SeverityLevel } from './../services/logger';
 
-export class PacketIO {
+export class PacketIO extends events.EventEmitter {
 
     private sendRC4: RC4;
+    private receiveRC4: RC4;
+    private socket: net.Socket;
 
-    constructor() {
+    constructor(socket: net.Socket) {
+        super();
+        this.socket = socket;
         this.sendRC4 = new RC4(OUTGOING_KEY);
+        this.receiveRC4 = new RC4(INCOMING_KEY);
+
+        socket.on('data', (data: Buffer) => {
+            if (data.length < 5) {
+                Log('PacketIO', 'Received failure packet.', SeverityLevel.Warning);
+                return;
+            }
+            const packetSize = data.readInt32BE(0);
+            const packetId = data.readInt8(4);
+            const packetData = this.receiveRC4.decipher(data.slice(5, data.length));
+
+            let packet;
+            try {
+                packet = Packets.create(packetId, packetSize - 5);
+                packet.data = packetData;
+            } catch (error) {
+                Log('PacketIO', error.message, SeverityLevel.Error);
+            }
+
+            if (packet) {
+                packet.read();
+                this.emit('packet', packet);
+            }
+        });
     }
 
-    sendPacket(packet: Packet): Buffer {
+    sendPacket(packet: Packet): void {
         packet.reset();
         packet.write();
         let packetSize = packet.data.length;
@@ -22,6 +54,6 @@ export class PacketIO {
         packet.writeInt32(packetSize);
         packet.writeByte(packet.id);
 
-        return packet.data;
+        this.socket.write(packet.data);
     }
 }
