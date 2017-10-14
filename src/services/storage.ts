80c3137328fbbc64ea28aad8b05c72e2dc674617
thomas-crane <thomasgcrane@gmail.com>
2017-10-14 12:23:17 +1100

    Added storage.ts service class for persisting data

---
+++ src/services/storage.ts
@@ -0,0 +1,28 @@
+import fs = require('fs');
+import path = require('path');
+
+export class Storage {
+    public static get(file: string): Promise<any> {
+        return new Promise((resolve, reject) => {
+            fs.readFile(path.resolve(__dirname, file), 'utf8', (error, data) => {
+                if (error) {
+                    reject(error);
+                    return;
+                }
+                resolve(JSON.parse(data));
+            });
+        });
+    }
+
+    public static set(file: string, data: object): Promise<any> {
+        return new Promise((resolve, reject) => {
+            fs.writeFile(path.resolve(__dirname, file), JSON.stringify(data), (error) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve();
+                }
+            });
+        });
+    }
+}
