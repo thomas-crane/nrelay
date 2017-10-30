b5be7fbc79a514a3c960b36c81e995e85d942889
thomas-crane <thomasgcrane@gmail.com>
2017-10-30 16:41:54 +1100

    Implemented RC4 cipher
    The node implementation of RC4 seems to have trouble maintaning the
    Keystream state, so a custom implementation was required. This has fixed
    all issues with state persistence.
    
    This implementation was created by translating the pseudo-code on the
    RC4 wikipedia page into TypeScript.

--- src/crypto/rc4.ts
+++ src/crypto/rc4.ts
@@ -2,31 +2,72 @@ import crypto = require('crypto');
 
 export class RC4 {
 
-    private key: string;
-    private encrypt: crypto.Cipher;
-    private decrypt: crypto.Decipher;
+    private state: number[];
+    private key: Buffer;
+    private i: number;
+    private j: number;
 
-    constructor(key: string) {
-        this.encrypt = crypto.createCipheriv('rc4', Buffer.from(key, 'hex'), '');
-        this.decrypt = crypto.createDecipheriv('rc4', Buffer.from(key, 'hex'), '');
+    /**
+     * Constructs a new RC4 cipher object and initializes
+     * the Keystream State with the given key.
+     * @param key The key to use in the Keystream.
+     */
+    constructor (key: Buffer) {
+        this.key = key;
+        this.reset();
     }
 
-    public cipher(data: Buffer): Buffer {
-        const sealedBuffer = [];
-        sealedBuffer.push(this.encrypt.update(data));
-        sealedBuffer.push(this.encrypt.final());
-
-        return Buffer.concat(sealedBuffer, sealedBuffer[0].length + sealedBuffer[1].length);
+    /**
+     * Performs an inline XOR on all bits in the data buffer with
+     * the next bit of the Keystream.
+     * @param data A stream of data to cipher using the Keystream.
+     */
+    public cipher(data: Buffer): void {
+        for (let n = 0; n < data.length; n++) {
+            this.i = (this.i + 1) % 256;
+            this.j = (this.j + this.state[this.i]) % 256;
+            const tmp = this.state[this.i];
+            this.state[this.i] = this.state[this.j];
+            this.state[this.j] = tmp;
+            const k = this.state[(this.state[this.i] + this.state[this.j]) % 256];
+            data[n] = (data[n] ^ k);
+        }
     }
 
-    public decipher(data: Buffer) {
-        const sealedBuffer = [];
-        sealedBuffer.push(this.decrypt.update(data));
-        sealedBuffer.push(this.decrypt.final());
+    /**
+     * Initializes the Keystream State.
+     */
+    private reset(): void {
+        this.state = new Array(256);
+        this.i = 0;
+        this.j = 0;
+        for (let i = 0; i < 256; i++) {
+            this.state[i] = i;
+        }
 
-        return Buffer.concat(sealedBuffer, sealedBuffer[0].length + sealedBuffer[1].length);
+        let j = 0;
+        for (let i = 0; i < 256; i++) {
+            j = (j + this.state[i] + this.key[i % this.key.length]) % 256;
+            const tmp = this.state[i];
+            this.state[i] = this.state[j];
+            this.state[j] = tmp;
+        }
     }
 }
 
+/**
+ * The RC4 Private Key used to encrypt outgoing packet.
+ * This key is a Hex String, so should be converted to
+ * a Buffer for use.
+ * @example
+ * const key = Buffer.from(OUTGOING_KEY, 'hex');
+ */
 export const OUTGOING_KEY = '311f80691451c71d09a13a2a6e';
+/**
+ * The RC4 Private Key to decrypt incoming packet data.
+ * This key is a Hex String, so should be converted to
+ * a Buffer for use.
+ * @example
+ * const key = Buffer.from(INCOMING_KEY, 'hex');
+ */
 export const INCOMING_KEY = '72c5583cafb6818995cdd74b80';
