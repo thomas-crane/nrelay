f26b95d1cac96b56f22129e5c36f86fa67193fd4
thomas-crane <thomasgcrane@gmail.com>
2017-10-30 16:45:52 +1100

    Added Failure packet

---
+++ src/networking/packets/incoming/failure-packet.ts
@@ -0,0 +1,23 @@
+import { Packet, PacketType } from '../../packet';
+
+export class FailurePacket extends Packet {
+
+    public type = PacketType.Failure;
+
+    //#region packet-specific members
+    errorId: number;
+    errorDescription: string;
+    //#endregion
+
+    data: Buffer;
+
+    public read(): void {
+        this.errorId = this.readInt32();
+        this.errorDescription = this.readString();
+    }
+
+    public write(): void {
+        this.writeInt32(this.errorId);
+        this.writeString(this.errorDescription);        
+    }
+}
