49bd1fa7ce84265c53d995ad5e1b625626b66aba
thomas-crane <thomasgcrane@gmail.com>
2017-11-01 16:09:01 +1100

    Added PlayerData interface

---
+++ src/models/playerdata.ts
@@ -0,0 +1,49 @@
+export interface IPlayerData {
+    charId: number;
+    nextCharId: number;
+    maxNumChars: number;
+    name: string;
+    level: number;
+    exp: number;
+    currentFame: number;
+    maxHP: number;
+    maxMP: number;
+    hp: number;
+    mp: number;
+    atk: number;
+    def: number;
+    spd: number;
+    dex: number;
+    wis: number;
+    vit: number;
+    hpPots: number;
+    mpPots: number;
+    hasBackpack: boolean;
+    inventory: { [id: number]: number };
+}
+
+export function getDefaultPlayerData(): IPlayerData {
+    return {
+        charId: 0,
+        nextCharId: 0,
+        maxNumChars: 0,
+        name: '',
+        level: 0,
+        exp: 0,
+        currentFame: 0,
+        maxHP: 0,
+        maxMP: 0,
+        hp: 0,
+        mp: 0,
+        atk: 0,
+        def: 0,
+        spd: 0,
+        dex: 0,
+        wis: 0,
+        vit: 0,
+        hpPots: 0,
+        mpPots: 0,
+        hasBackpack: false,
+        inventory: {}
+    };
+}
