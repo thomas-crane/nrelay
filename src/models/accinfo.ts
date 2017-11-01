9f211b4d4d6ed07e93b82410ef13af493de3b23e
thomas-crane <thomasgcrane@gmail.com>
2017-11-01 17:46:09 +1100

    Added accinfo model

---
+++ src/models/accinfo.ts
@@ -0,0 +1,9 @@
+export interface IAccountInfo {
+    buildVersion: string;
+    guid: string;
+    password: string;
+    serverPref: string;
+    charId: number;
+    nextCharId: number;
+    maxNumChars: number;
+}
