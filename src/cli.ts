523f06ce2fdfa8bd5065e26f835fb0e7b00b8093
thomas-crane <thomasgcrane@gmail.com>
2017-11-01 17:49:25 +1100

    Reworked CLI to load acc config

--- src/cli.ts
+++ src/cli.ts
@@ -1,27 +1,48 @@
 import { Log, SeverityLevel } from './services/logger';
 import { Http } from './services/http';
-import { parseServers } from './services/xmltojson';
+import { parseServers, parseAccountInfo } from './services/xmltojson';
 import { SERVER_ENDPOINT } from './models/api-endpoints';
 import { IServer } from './models/server';
-import readline = require('readline');
+import { IAccountInfo } from './models/accinfo';
 import { Client } from './core/client';
+import { Storage } from './services/storage';
 
 export class CLI {
 
-    private serverList: IServer[];
+    private serverList: { [id: string]: IServer };
 
     constructor() {
         Log('NRelay', 'Starting...', SeverityLevel.Info);
-        this.getServers().then((servers) => {
-            this.serverList = servers;
-            if (servers.length > 0) {
-                Log('NRelay', 'Connecting to ' + servers[4].name);
-                const client = new Client(servers[4].address);
+        const accInfo = Storage.getAccountConfig();
+        if (!accInfo) {
+            Log('NRelay', 'Couldn\'t load acc-config.json.', SeverityLevel.Error);
+            return;
+        }
+
+        this.getAccountInfo(accInfo.guid, accInfo.password).then((info) => {
+            const keys = Object.keys(this.serverList);
+            if (keys.length > 0) {
+                let server: IServer;
+                if (!this.serverList[accInfo.serverPref]) {
+                    Log('NRelay', 'Preferred server not found. Choosing first server.', SeverityLevel.Warning);
+                    server = this.serverList[keys[0]];
+                } else {
+                    server = this.serverList[accInfo.serverPref];
+                }
+                Log('NRelay', 'Connecting to ' + server.name);
+                info.buildVersion = accInfo.buildVersion;
+                info.guid = accInfo.guid;
+                info.password = accInfo.password;
+                const client = new Client(server.address, info);
+            } else {
+                Log('NRelay', 'Couldn\'t get servers.', SeverityLevel.Error);
             }
+        }).catch((err) => {
+
         });
     }
 
-    getServers(): Promise<IServer[]> {
+    getServers(): Promise<{ [id: string]: IServer }> {
         Log('NRelay', 'Getting server list...', SeverityLevel.Info);
         return new Promise((resolve, reject) => {
             Http.get(SERVER_ENDPOINT).then((data) => {
@@ -32,6 +53,26 @@ export class CLI {
                 Log('NRelay', 'Error getting servers.', SeverityLevel.Error);
                 reject(serverListError);
             });
+        });
+    }
+
+    getAccountInfo(guid: string, password: string): Promise<IAccountInfo> {
+        return new Promise((resolve, reject) => {
+            Http.get(SERVER_ENDPOINT, {
+                guid: guid,
+                password: password
+            }).then((data) => {
+                const info = parseAccountInfo(data);
+                this.serverList = parseServers(data);
+                if (info) {
+                    Log('NRelay', 'Authorized account', SeverityLevel.Success);
+                    resolve(info);
+                } else {
+                    console.log(data);
+                    Log('NRelay', 'Error authorizing account', SeverityLevel.Warning);
+                    reject();
+                }
+            }).catch((err) => reject(err));
         });
     }
 }
