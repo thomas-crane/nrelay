8ba0bccb89d6fa296936dcf000450e4e2fa8609d
thomas-crane <thomasgcrane@gmail.com>
2017-11-01 16:47:28 +1100

    Modified http service to allow query strings

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,9 +1,22 @@
 import http = require('http');
 
 export class Http {
-    static get(path: string): Promise<any> {
+    static get(path: string, query?: { [id: string]: string }): Promise<any> {
         return new Promise((resolve, reject) => {
-            http.get(path, (response) => {
+            let qs = '';
+            const keys = Object.keys(query);
+            for (let i = 0; i < keys.length; i++) {
+                if (i === 0) {
+                    qs += '?';
+                }
+                qs += encodeURI(keys[i]);
+                qs += '=';
+                qs += encodeURI(query[keys[i]]);
+                if (i !== keys.length - 1) {
+                    qs += '&';
+                }
+            }
+            http.get(path + qs, (response) => {
                 response.setEncoding('utf8');
                 let data = '';
                 response.on('data', (chunk) => {
