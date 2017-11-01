aa3e1aca59bdf616fa0c2743ceaab64fa4c7755c
thomas-crane <thomasgcrane@gmail.com>
2017-11-01 16:47:08 +1100

    Added parseAccountInfo method to xmltojson

--- src/services/xmltojson.ts
+++ src/services/xmltojson.ts
@@ -1,6 +1,8 @@
-import { IServer } from '../models/server';
+import { IServer } from './../models/server';
 
 const SERVER_REGEX = /<Server><Name>(\w+)<\/Name><DNS>(\d+\.\d+\.\d+\.\d+)<\/DNS>/g;
+
+const ACCOUNT_INFO_REGEX = /<Chars nextCharId="(\d+)" maxNumChars="(\d+)"><Char id="(\d+)">/g;
 
 export class XMLtoJSON {
 
@@ -19,4 +21,21 @@ export function parseServers(xml: string): IServer[] {
         match = SERVER_REGEX.exec(xml);
     }
     return servers;
+}
+
+export function parseAccountInfo(xml: string): { nextCharId: number, charId: number, maxNumChars: number } | null {
+    const info = {
+        nextCharId: 2,
+        charId: 1,
+        maxNumChars: 1,
+    };
+    const match = ACCOUNT_INFO_REGEX.exec(xml);
+    if (match != null) {
+        info.nextCharId = +match[1];
+        info.maxNumChars = +match[2];
+        info.charId = +match[3];
+    } else {
+        return null;
+    }
+    return info;
 }
