8f5239d6a55d9aa49d95dd755ed89f99ec89a1cc
thomas-crane <thomasgcrane@gmail.com>
2017-11-01 16:10:23 +1100

    Added ping packet

---
+++ src/networking/packets/incoming/ping-packet.ts
@@ -0,0 +1,18 @@
+import { Packet, PacketType } from '../../packet';
+
+export class PingPacket extends Packet {
+
+    public type = PacketType.Ping;
+
+    //#region packet-specific members
+    serial: number;
+    //#endregion
+
+    public read(): void {
+        this.serial = this.readInt32();
+    }
+
+    public write(): void {
+        this.writeInt32(this.serial);
+    }
+}
