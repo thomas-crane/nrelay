39f0b4f127ca996812add4d342118d1f29ed58f7
thomas-crane <thomasgcrane@gmail.com>
2017-11-01 17:48:36 +1100

    Removed old log message

--- src/decorators/hook-packet.ts
+++ src/decorators/hook-packet.ts
@@ -5,7 +5,6 @@ import { PluginManager } from './../core/plugin-manager';
 export function HookPacket(type: PacketType): (target: object, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) => void {
     return (target: object, propertyKey: string, descriptor: TypedPropertyDescriptor<any>): void => {
         const originalMethod = descriptor.value;
-        Log('Hooks', 'Hooking packet type: ' + type, SeverityLevel.Info);
         PluginManager.addHook(type, originalMethod);
     };
 }
