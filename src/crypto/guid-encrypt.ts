718605ed2233c395c089446a8c6c8dc000820b1e
thomas-crane <thomasgcrane@gmail.com>
2017-11-01 16:49:01 +1100

    Switched from node rsa implementation to node-rsa npm module

--- src/crypto/guid-encrypt.ts
+++ src/crypto/guid-encrypt.ts
@@ -1,15 +1,22 @@
 import crypto = require('crypto');
+import NodeRSA = require('node-rsa');
+
+const PUBLIC_KEY =
+'-----BEGIN PUBLIC KEY-----\n' +
+'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDCKFctVrhfF3m2Kes0FBL/JFeO' +
+'cmNg9eJz8k/hQy1kadD+XFUpluRqa//Uxp2s9W2qE0EoUCu59ugcf/p7lGuL99Uo' +
+'SGmQEynkBvZct+/M40L0E0rZ4BVgzLOJmIbXMp0J4PnPcb6VLZvxazGcmSfjauC7' +
+'F3yWYqUbZd/HCBtawwIDAQAB\n' +
+'-----END PUBLIC KEY-----';
 
 export function encryptGUID(guid: string): string {
-    const key =
-    '-----BEGIN PUBLIC KEY-----\n' +
-    'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDCKFctVrhfF3m2Kes0FBL/JFeO\n' +
-    'cmNg9eJz8k/hQy1kadD+XFUpluRqa//Uxp2s9W2qE0EoUCu59ugcf/p7lGuL99Uo\n' +
-    'SGmQEynkBvZct+/M40L0E0rZ4BVgzLOJmIbXMp0J4PnPcb6VLZvxazGcmSfjauC7\n' +
-    'F3yWYqUbZd/HCBtawwIDAQAB\n' +
-    '-----END PUBLIC KEY-----';
+    if (guid.trim() === '') {
+        return '';
+    }
 
-    const encryptedGUID = crypto.publicEncrypt(key, Buffer.from(guid, 'utf8'));
+    const key = new NodeRSA(PUBLIC_KEY, 'pkcs8-public', {
+        encryptionScheme: 'pkcs1'
+    });
 
-    return encryptedGUID.toString('base64');
+    return key.encrypt(Buffer.from(guid, 'utf8'), 'base64', 'utf8');
 }
