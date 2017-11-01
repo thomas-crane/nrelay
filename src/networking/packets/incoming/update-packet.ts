9d4ba280aad98da732114749a768aad03cb024f0
thomas-crane <thomasgcrane@gmail.com>
2017-11-01 16:10:38 +1100

    Added update packet

---
+++ src/networking/packets/incoming/update-packet.ts
@@ -0,0 +1,58 @@
+import { Packet, PacketType } from '../../packet';
+import { GroundTileData } from './../../data/ground-tile-data';
+import { ObjectData } from '../../data/object-data';
+
+export class UpdatePacket extends Packet {
+
+    public type = PacketType.Update;
+
+    //#region packet-specific members
+    tiles: GroundTileData[];
+    newObjects: ObjectData[];
+    drops: number[];
+    //#endregion
+
+    data: Buffer;
+
+    public read(): void {
+        const tilesLen = this.readShort();
+        this.tiles = new Array<GroundTileData>(tilesLen);
+        for (let i = 0; i < tilesLen; i++) {
+            const gd = new GroundTileData();
+            gd.read(this);
+            this.tiles[i] = gd;
+        }
+
+        const newObjectsLen = this.readShort();
+        this.newObjects = new Array<ObjectData>(newObjectsLen);
+        for (let i = 0; i < newObjectsLen; i++) {
+            const od = new ObjectData();
+            od.read(this);
+            this.newObjects[i] = od;
+        }
+
+        const dropsLen = this.readShort();
+        this.drops = new Array<number>(dropsLen);
+        for (let i = 0; i < dropsLen; i++) {
+            const drop = this.readInt32();
+            this.drops[i] = drop;
+        }
+    }
+
+    public write(): void {
+        this.writeShort(this.tiles.length);
+        for (let i = 0; i < this.tiles.length; i++) {
+            this.tiles[i].write(this);
+        }
+
+        this.writeShort(this.newObjects.length);
+        for (let i = 0; i < this.newObjects.length; i++) {
+            this.newObjects[i].write(this);
+        }
+
+        this.writeShort(this.drops.length);
+        for (let i = 0; i < this.drops.length; i++) {
+            this.writeInt32(this.drops[i]);
+        }
+    }
+}
