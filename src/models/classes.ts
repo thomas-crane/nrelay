5483e21e03aca350aea0c2fdd57bebe28b9e54fa
thomas-crane <thomasgcrane@gmail.com>
2017-11-02 23:27:57 +1100

    Added classes enum

---
+++ src/models/classes.ts
@@ -0,0 +1,16 @@
+export enum Classes {
+    Rogue = 768,
+    Archer = 775,
+    Wizard = 782,
+    Priest = 784,
+    Warrior = 797,
+    Knight = 798,
+    Paladin = 799,
+    Assassin = 800,
+    Necromancer = 801,
+    Huntress = 802,
+    Mystic = 803,
+    Trickster = 804,
+    Sorcerer = 805,
+    Ninja = 806
+}
