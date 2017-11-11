8288ffd1f5aa97333d892d6b2749891af0b3ab3e
thomas-crane <thomasgcrane@gmail.com>
2017-11-11 20:22:44 +1100

    Changed property style of walkpath plugin

--- src/plugins/walkpath-plugin.ts
+++ src/plugins/walkpath-plugin.ts
@@ -14,27 +14,29 @@ const path = [
     { x: 125.1288, y: 118.0530 }  // top-left
 ];
 
-const players: {
-    [id: number]: any[]
-} = {};
-
 @NrPlugin({
     name: 'Path Walker',
     author: 'tcrane'
 })
-export default class YourPluginName {
+export default class WalkpathPlugin {
+
+    private players: {
+        [id: number]: any[]
+    };
+
     constructor() {
+        this.players = {};
     }
 
     @HookPacket(PacketType.NewTick)
     onNewTick(client: Client, newTickPacket: NewTickPacket): void {
-        if (!players[client.playerData.objectId]) {
-            players[client.playerData.objectId] = path.slice();
+        if (!this.players[client.playerData.objectId]) {
+            this.players[client.playerData.objectId] = path.slice();
         }
         if (!client.nextPos) {
             const wp = new WorldPosData();
-            const point = players[client.playerData.objectId].pop();
-            players[client.playerData.objectId].unshift(point);
+            const point = this.players[client.playerData.objectId].pop();
+            this.players[client.playerData.objectId].unshift(point);
             wp.x = point.x;
             wp.y = point.y;
             client.nextPos = wp;
