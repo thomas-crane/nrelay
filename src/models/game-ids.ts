3d46aebeaca1af59409a9726e46c539178a6b2bd
thomas-crane <thomasgcrane@gmail.com>
2018-09-24 09:23:26 +0300

    Move game-ids to appropriate folder

---
+++ src/models/game-ids.ts
@@ -0,0 +1,8 @@
+/**
+ * The game IDs for various maps which the client can connect to.
+ */
+export enum GameId {
+  Nexus = -2,
+  RandomRealm = -3,
+  Vault = -5
+}
--- src/networking/game-ids.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Static gameIds for various, switching gameId and connecting allows you to connect to the map.
- */
-export enum GameId {
-    NEXUS = -2,
-    RANDOMREALM = -3,
-    VAULT = -5
-}
