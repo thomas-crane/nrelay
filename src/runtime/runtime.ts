14cf8c2fb5000205fe193f4fa4a44c2a123a9362
thomas-crane <thomasgcrane@gmail.com>
2019-03-30 00:46:51 +1100

    feat(runtime): add runtime
    This commit adds the Runtime class to nrelay. The runtime is a new
    concept for nrelay which is designed to change the way nrelay is used.
    At the moment, you have to download the source, add your plugins, and
    build it yourself. This is great if you want to hack on the source, but
    terrible if you just want to make a quick new project and start writing
    plugins. The runtime can be created and pointed to a folder which
    represents a "project". The project folder is expected to have various
    files in it suchs as the accounts.json, versions.json, etc, and the
    plugin files. This means that nrelay can be installed as an npm
    dependency into a standalone project, and then a runtime can just be
    created from that project.

---
+++ src/runtime/runtime.ts
@@ -0,0 +1,205 @@
+import { createWriteStream, WriteStream } from 'fs';
+import { Client, ResourceManager } from '../core';
+import { Account, Server } from '../models';
+import { PacketMap } from '../networking/packet-map';
+import { AccountService, ArgsResult, DefaultLogger, FileLogger, Logger, LogLevel, StringUtils, Updater } from '../services';
+import { Environment } from './environment';
+import { Versions } from './versions';
+
+/**
+ * The runtime manages clients, resources, plugins and any other services
+ * which are used by an nrelay project.
+ */
+export class Runtime {
+
+  /**
+   * The environment of this runtime.
+   */
+  readonly env: Environment;
+
+  /**
+   * The updater used by this runtime.
+   */
+  readonly updater: Updater;
+  /**
+   * The account service used by this runtime.
+   */
+  readonly accountService: AccountService;
+  /**
+   * The resource manager used by this runtime.
+   */
+  readonly resources: ResourceManager;
+  /**
+   * The clients which are managed by this runtime.
+   */
+  readonly clients: Map<string, Client>;
+  /**
+   * A bidirectional map of packet ids.
+   */
+  packetMap: PacketMap;
+  /**
+   * The build version to use when creating new clients.
+   */
+  buildVersion: string;
+
+  /**
+   * A WriteStream which is used for the log file.
+   */
+  private logStream: WriteStream;
+
+  constructor(environment: Environment) {
+    this.env = environment;
+    this.updater = new Updater(this.env);
+    this.accountService = new AccountService(this.env);
+    this.resources = new ResourceManager(this.env);
+    this.clients = new Map();
+  }
+
+  /**
+   * Starts this runtime.
+   * @param args The arguments to start the runtime with.
+   */
+  async run(args: ArgsResult): Promise<void> {
+    // if we want to get the version, just print it and exit.
+    if (args.version || args.v) {
+      const nrelayVersion = require('../../package.json').version;
+      console.log(`v${nrelayVersion}`);
+      process.exit(0);
+    }
+
+    // set up the logging.
+    let minLevel = LogLevel.Info;
+    if (args.debug) {
+      minLevel = LogLevel.Debug;
+    }
+    Logger.addLogger(new DefaultLogger(minLevel));
+
+    // set up the log file if we have the flag enabled.
+    if (args.log) {
+      this.createLog();
+      Logger.addLogger(new FileLogger(this.logStream));
+    }
+
+    // force an update.
+    if (args['force-update']) {
+      try {
+        await this.updater.performUpdate({ needAssetUpdate: true, needClientUpdate: true });
+        Logger.log('Runtime', 'Finished updating!', LogLevel.Success);
+      } catch (err) {
+        Logger.log('Runtime', `Error while updating: ${err.message}`, LogLevel.Error);
+      }
+    } else {
+      // just check for updates normally (as long as --no-update wasn't included).
+      if (args.update !== false) {
+        try {
+          Logger.log('Runtime', 'Checking for updates...', LogLevel.Info);
+          const updateInfo = await this.updater.checkForUpdates();
+          await this.updater.performUpdate(updateInfo);
+          Logger.log('Runtime', 'Done!', LogLevel.Success);
+        } catch (err) {
+          Logger.log('Runtime', `Error while updating: ${err.message}`, LogLevel.Error);
+        }
+      }
+    }
+
+    // load the resources.
+    try {
+      this.resources.loadAllResources();
+    } catch (error) {
+      Logger.log('Runtime', 'Error while loading resources.', LogLevel.Error);
+      Logger.log('Runtime', error.message, LogLevel.Error);
+      process.exit(0);
+    }
+
+    // load the packets
+    const packets: PacketMap = this.env.readJSON('packets.json');
+    if (!packets) {
+      Logger.log('Runtime', 'Cannot load packets.json', LogLevel.Error);
+      process.exit(0);
+    } else {
+      this.packetMap = packets;
+      // the length is divided by 2 because the map is bidirectional.
+      const size = Object.keys(this.packetMap).length / 2;
+      Logger.log('Runtime', `Mapped ${size} packet ids.`, LogLevel.Info);
+    }
+
+    // load the buildVersion.
+    const versions = this.env.readJSON<Versions>('versions.json');
+    if (versions && versions.buildVersion) {
+      this.buildVersion = versions.buildVersion;
+      Logger.log('Runtime', `Using build version "${this.buildVersion}"`, LogLevel.Info);
+    } else {
+      Logger.log('Runtime', 'Cannot load buildVersion. It will be loaded when a client connects.', LogLevel.Warning);
+    }
+
+    // finally, load any accounts.
+    const accounts = this.env.readJSON<Account[]>('accounts.json');
+    if (accounts) {
+      for (const account of accounts) {
+        this.addClient(account);
+      }
+    }
+  }
+
+  /**
+   * Creates a new client which uses the provided account.
+   * @param account The account to login to.
+   */
+  addClient(account: Account): Promise<Client> {
+    // make sure the client has an alias.
+    if (!account.alias) {
+      account.alias = StringUtils.censorGuid(account.guid);
+    }
+
+    // make sure it's not already part of this runtime.
+    if (this.clients.has(account.guid)) {
+      return Promise.reject(new Error(`The account "${account.alias}" is already managed by this runtime.`));
+    }
+
+    Logger.log('Runtime', `Loading ${account.alias}...`);
+
+    // get the server list and char info.
+    return Promise.all([
+      this.accountService.getServerList(),
+      this.accountService.getCharacterInfo(account.guid, account.password, account.proxy),
+    ]).then(([servers, charInfo]) => {
+      account.charInfo = charInfo;
+
+      // make sure the server exists.
+      let server: Server;
+      if (servers[account.serverPref]) {
+        server = servers[account.serverPref];
+      } else {
+        const keys = Object.keys(servers);
+        if (keys.length === 0) {
+          throw new Error('Server list is empty.');
+        }
+        server = servers[keys[Math.floor(Math.random() * keys.length)]];
+        Logger.log(account.alias, `Preferred server not found. Using ${server.name} instead.`, LogLevel.Warning);
+      }
+      Logger.log('Runtime', `Loaded ${account.alias}!`, LogLevel.Success);
+      const client = new Client(this, server, account);
+      this.clients.set(client.guid, client);
+      return client;
+    });
+  }
+
+  /**
+   * Creates a log file for this runtime.
+   */
+  private createLog(): void {
+    const nrelayVersion = require('../../package.json').version;
+    this.logStream = createWriteStream(this.env.pathTo('nrelay-log.log'));
+    const watermark = [
+      'INFO',
+      '----',
+      `date           :: ${(new Date()).toString()}`,
+      `nrelay version :: v${nrelayVersion}`,
+      `node version   :: ${process.version}`,
+      '',
+      'LOG',
+      '----',
+    ].join('\n');
+    this.logStream.write(`${watermark}\n`);
+  }
+}
