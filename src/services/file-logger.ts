b6f9ea3d715c551383df2ee4436a55243fab673e
thomas-crane <thomasgcrane@gmail.com>
2019-04-23 19:00:09 +1000

    feat(string-utils): remove StringUtils class
    This commit replaces the static clas StringUtils class with a file
    containing exported top level functions that used to be part of the
    string utils class.

--- src/services/file-logger.ts
+++ src/services/file-logger.ts
@@ -3,7 +3,7 @@
  */
 import { WriteStream } from 'fs';
 import { LogLevel, LogProvider } from './logger';
-import { StringUtils } from './string-utils';
+import * as stringUtils from './string-utils';
 
 /**
  * A logger which writes log messages to a `WriteStream`.
@@ -13,12 +13,12 @@ export class FileLogger implements LogProvider {
   constructor(private logStream: WriteStream) { }
 
   log(sender: string, message: string, level: LogLevel): void {
-    const senderString = (`[${StringUtils.getTime()} | ${sender}]`);
-    const printString: string = StringUtils.pad(senderString, 30) + message;
+    const senderString = (`[${stringUtils.getTime()} | ${sender}]`);
+    const printString: string = stringUtils.pad(senderString, 30) + message;
     let levelString = LogLevel[level];
     if (!levelString) {
       levelString = 'custom';
     }
-    this.logStream.write(StringUtils.pad(levelString.toUpperCase(), 8) + printString + '\n');
+    this.logStream.write(stringUtils.pad(levelString.toUpperCase(), 8) + printString + '\n');
   }
 }
