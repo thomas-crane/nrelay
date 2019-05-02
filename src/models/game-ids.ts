b9e7befc3ebb60c258e148dcc07767ec40f15847
thomas-crane <thomasgcrane@gmail.com>
2019-05-02 17:25:06 +1000

    chore(game-id): add more game ids
    This commit adds several new game id values to the GameId enum.

--- src/models/game-ids.ts
+++ src/models/game-ids.ts
@@ -5,7 +5,13 @@
  * The game IDs for various maps which the client can connect to.
  */
 export enum GameId {
+  Tutorial = -1,
   Nexus = -2,
   RandomRealm = -3,
+  NexusTutorial = -4,
   Vault = -5,
+  MapTest = -6,
+  VaultExplanation = -8,
+  NexusExplanation = -9,
+  QuestRoom = -11,
 }
