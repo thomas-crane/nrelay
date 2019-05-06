086cd34918b681dcff39e29c1d67b01f38b9539e
thomas-crane <thomasgcrane@gmail.com>
2019-05-06 22:57:27 +1000

    feat(models): refactor enemy model
    This commit refactors the enemy model into an Entity base class and an
    Enemy superclass.

--- src/models/enemy.ts
+++ src/models/enemy.ts
@@ -1,20 +1,14 @@
 /**
  * @module models
  */
-import { ObjectStatusData } from '@realmlib/net';
-import { Point } from '@realmlib/net';
-import * as parsers from '../util/parsers';
+import { ObjectStatusData, Point } from '@realmlib/net';
+import { Entity } from './entity';
 import { GameObject } from './object';
-import { PlayerData } from './playerdata';
 
 /**
  * An enemy game object.
  */
-export class Enemy {
-  /**
-   * Stat information of the enemy, such as HP and defense.
-   */
-  objectData: PlayerData;
+export class Enemy extends Entity {
   /**
    * The properties of this enemy as described by the Objects resource.
    */
@@ -23,110 +17,15 @@ export class Enemy {
    * Whether or not this enemy is dead.
    */
   dead: boolean;
-  /**
-   * The client time of the last update that this enemy received.
-   */
-  lastUpdate: number;
-  /**
-   * The position of the enemy as received in a `NewTick` packet.
-   */
-  tickPos: Point;
-  /**
-   * The position of the enemy at a particular game tick.
-   */
-  posAtTick: Point;
-  /**
-   * The velocity of the enemy.
-   */
-  moveVector: Point;
-  /**
-   * The tick id of the last game tick received by this enemy.
-   */
-  lastTickId: number;
-  /**
-   * The current position of the enemy.
-   */
-  currentPos: Point;
 
   constructor(properties: GameObject, status: ObjectStatusData) {
+    super(status);
     this.properties = properties;
-    this.objectData = parsers.processObjectStatus(status);
-    this.dead = false;
-    this.lastUpdate = 0;
-    this.lastTickId = -1;
-    this.currentPos = status.pos.clone();
-    this.tickPos = {
-      x: this.currentPos.x,
-      y: this.currentPos.y,
-    };
-    this.posAtTick = {
-      x: this.currentPos.x,
-      y: this.currentPos.y,
-    };
-    this.moveVector = {
-      x: 0,
-      y: 0,
-    };
-  }
-
-  onNewTick(objectStatus: ObjectStatusData, tickTime: number, tickId: number, clientTime: number): void {
-    this.objectData = parsers.processObjectStatus(objectStatus, this.objectData);
-    if (this.lastTickId < tickId) {
-      this.moveTo(this.tickPos.x, this.tickPos.y);
-    }
-    this.lastUpdate = clientTime;
-    this.tickPos.x = objectStatus.pos.x;
-    this.tickPos.y = objectStatus.pos.y;
-    this.posAtTick.x = this.currentPos.x;
-    this.posAtTick.y = this.currentPos.y;
-    this.moveVector = {
-      x: (this.tickPos.x - this.posAtTick.x) / tickTime,
-      y: (this.tickPos.y - this.posAtTick.y) / tickTime,
-    };
-    this.lastTickId = tickId;
-    this.lastUpdate = clientTime;
-  }
-
-  squareDistanceTo<T extends Point>(point: T): number {
-    const a = point.x - this.currentPos.x;
-    const b = point.y - this.currentPos.y;
-    return a ** 2 + b ** 2;
   }
 
   damage(damage: number): number {
     const min = damage * 3 / 20;
     const actualDamge = Math.max(min, damage - this.objectData.def);
     return actualDamge;
-  }
-
-  frameTick(lastTick: number, clientTime: number): void {
-    if (!(this.moveVector.x === 0 && this.moveVector.y === 0)) {
-      if (this.lastTickId < lastTick) {
-        this.moveVector.x = 0;
-        this.moveVector.y = 0;
-        this.moveTo(this.tickPos.x, this.tickPos.y);
-      } else {
-        const time = clientTime - this.lastUpdate;
-        const dX = this.posAtTick.x + time * this.moveVector.x;
-        const dY = this.posAtTick.y + time * this.moveVector.y;
-        this.moveTo(dX, dY);
-      }
-    }
-  }
-
-  onGoto(x: number, y: number, time: number): void {
-    this.moveTo(x, y);
-    this.tickPos.x = x;
-    this.tickPos.y = y;
-    this.posAtTick.x = x;
-    this.posAtTick.y = y;
-    this.moveVector.x = 0;
-    this.moveVector.y = 0;
-    this.lastUpdate = time;
-  }
-
-  private moveTo(x: number, y: number): void {
-    this.currentPos.x = x;
-    this.currentPos.y = y;
   }
 }
---
+++ src/models/entity.ts
@@ -0,0 +1,135 @@
+/**
+ * @module models
+ */
+import { ObjectStatusData, Point } from '@realmlib/net';
+import * as parsers from '../util/parsers';
+import { PlayerData } from './playerdata';
+
+/**
+ * An entity which exists in the game.
+ */
+export class Entity {
+  /**
+   * Stat information of the entity, such as HP and defense.
+   */
+  objectData: PlayerData;
+  /**
+   * The client time of the last update that this entity received.
+   */
+  lastUpdate: number;
+  /**
+   * The position of the entity as received in a `NewTick` packet.
+   */
+  tickPos: Point;
+  /**
+   * The position of the entity at a particular game tick.
+   */
+  posAtTick: Point;
+  /**
+   * The velocity of the entity.
+   */
+  moveVector: Point;
+  /**
+   * The tick id of the last game tick received by this entity.
+   */
+  lastTickId: number;
+  /**
+   * The current position of the entity.
+   */
+  currentPos: Point;
+
+  constructor(status: ObjectStatusData) {
+    this.objectData = parsers.processObjectStatus(status);
+    this.lastUpdate = 0;
+    this.lastTickId = -1;
+    this.currentPos = status.pos.clone();
+    this.tickPos = {
+      x: this.currentPos.x,
+      y: this.currentPos.y,
+    };
+    this.posAtTick = {
+      x: this.currentPos.x,
+      y: this.currentPos.y,
+    };
+    this.moveVector = {
+      x: 0,
+      y: 0,
+    };
+  }
+
+  /**
+   * Updates the entity based on the tick info.
+   * @param objectStatus The object status of this entity.
+   * @param tickTime The time of this tick.
+   * @param tickId The tick id of this tick.
+   * @param clientTime The client time of this tick.
+   */
+  onNewTick(objectStatus: ObjectStatusData, tickTime: number, tickId: number, clientTime: number): void {
+    this.objectData = parsers.processObjectStatus(objectStatus, this.objectData);
+    if (this.lastTickId < tickId) {
+      this.moveTo(this.tickPos.x, this.tickPos.y);
+    }
+    this.lastUpdate = clientTime;
+    this.tickPos.x = objectStatus.pos.x;
+    this.tickPos.y = objectStatus.pos.y;
+    this.posAtTick.x = this.currentPos.x;
+    this.posAtTick.y = this.currentPos.y;
+    this.moveVector = {
+      x: (this.tickPos.x - this.posAtTick.x) / tickTime,
+      y: (this.tickPos.y - this.posAtTick.y) / tickTime,
+    };
+    this.lastTickId = tickId;
+    this.lastUpdate = clientTime;
+  }
+
+  /**
+   * Updates this entities state based on the current frame.
+   * @param lastTick The last tick id.
+   * @param clientTime The current client time.
+   */
+  frameTick(lastTick: number, clientTime: number): void {
+    if (!(this.moveVector.x === 0 && this.moveVector.y === 0)) {
+      if (this.lastTickId < lastTick) {
+        this.moveVector.x = 0;
+        this.moveVector.y = 0;
+        this.moveTo(this.tickPos.x, this.tickPos.y);
+      } else {
+        const time = clientTime - this.lastUpdate;
+        const dX = this.posAtTick.x + time * this.moveVector.x;
+        const dY = this.posAtTick.y + time * this.moveVector.y;
+        this.moveTo(dX, dY);
+      }
+    }
+  }
+
+  /**
+   * The square distance from some point to this entity.
+   */
+  squareDistanceTo<T extends Point>(point: T): number {
+    const a = point.x - this.currentPos.x;
+    const b = point.y - this.currentPos.y;
+    return a ** 2 + b ** 2;
+  }
+
+  /**
+   * Updates this entity based on a goto packet.
+   * @param x The X position to go to.
+   * @param y The Y position to go to.
+   * @param time The client time.
+   */
+  onGoto(x: number, y: number, time: number): void {
+    this.moveTo(x, y);
+    this.tickPos.x = x;
+    this.tickPos.y = y;
+    this.posAtTick.x = x;
+    this.posAtTick.y = y;
+    this.moveVector.x = 0;
+    this.moveVector.y = 0;
+    this.lastUpdate = time;
+  }
+
+  private moveTo(x: number, y: number): void {
+    this.currentPos.x = x;
+    this.currentPos.y = y;
+  }
+}
