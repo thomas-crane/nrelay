a202cc3d9ce4523cdc8c42da52b3c3ed06f523e1
zigzag2002 <YOUR EMAIL ADDRESS>
2019-07-14 15:09:29 -0400

    enum for pet abilities

---
+++ src/models/pet-abilities.ts
@@ -0,0 +1,11 @@
+export enum PetAbilities {
+    AttackClose = 0x192,
+    AttackFar = 0x195,
+    AttackMid = 0x194,
+    Decoy = 0x19a,
+    Electric = 0x196,
+    Heal = 0x197,
+    MagicHeal = 0x198,
+    RisingFury = 0x19b,
+    Savage = 0x199
+  }
