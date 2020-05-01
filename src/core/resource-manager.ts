2862299e9142713861f9368f14504250a23ad85e
iremnant <iremnant@tutanota.com>
2020-05-01 21:02:27 +0000

    fix(resource-manager): ignore duplicate Objects.json entries

--- src/core/resource-manager.ts
+++ src/core/resource-manager.ts
@@ -69,6 +69,9 @@ export class ResourceManager {
     let petCount = 0;
     let objectsArray: any[] = objects.Object;
     for (const current of objectsArray) {
+      if (this.objects[+current.type] != null) {
+        continue;
+      }
       try {
         this.objects[+current.type] = {
           type: +current.type,
